84dffa702baebd1453efc68a21cd7518a75c703a
Augusto Marques <augustolfmarques@gmail.com>
2026-05-03 22:25:16 -0300

    build: update GNOME button layout and add 2 flatpaks to recipes
    - Remove appmenu from window button layout (set to :minimize,maximize,close)
    - Add io.podman_desktop.PodmanDesktop and net.nokyan.Resources to standard recipes
    - Apply minor whitespace cleanup in standard.yml

--- files/scripts/install-gnome-packages.sh
+++ files/scripts/install-gnome-packages.sh
@@ -2,7 +2,6 @@
 set -eoux pipefail
 
 rpm-ostree install -y \
-    gnome-tweaks \
     gnome-shell-extension-dash-to-panel \
     gnome-shell-extension-blur-my-shell \
     gnome-shell-extension-caffeine
